a71dbbe144dd845dc2ed49a6112ba86704aadedb
shebin <mohammedshebinc92@gmail.com>
2025-01-30 10:00:12 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const { resolve } = require('path');
+require('dotenv').config()
 
 const app = express();
-const port = 3010;
+const PORT = 3010;
+
+let connection=mongoose.connect(process.env.mongoURL)
 
 app.use(express.static('static'));
 
@@ -10,6 +14,13 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, async() => {
+  try{
+    await connection;
+    console.log("Connected to database  ")
+}
+catch(error){
+    console.log(error)
+}
+console.log(`Server is running on port ${PORT}`)
 });
